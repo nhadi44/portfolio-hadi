0e44ba13357d005d4349604831fb83d2217e4624
Hadi Nurhidayat <75003588+nhadi44@users.noreply.github.com>
2023-12-28 20:43:16 +0700

    create about section

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,18 +1,27 @@
 import DefaultLayout from "../Layout/Default";
 import Section from "@/components/partials/Section/index.jsx";
 import Hero from "@/components/partials/Hero/index.jsx";
+import About from "@/components/partials/About/index.jsx";
+import PropTypes from "prop-types";
 
-const Homepage = ({title}) => {
-
+const Homepage = ({ title }) => {
     return (
         <DefaultLayout title={title}>
             <Section classname={"h-screen"} sectionId={"hero"}>
-                <Hero/>
+                <Hero />
             </Section>
-            <Section classname={"h-screen bg-[#000201]"} sectionId={"about"}>About</Section>
-            <Section classname={"h-screen"} sectionId={"experience"}>Experience</Section>
+            <Section classname={"h-screen bg-[#000201]"} sectionId={"about"}>
+                <About />
+            </Section>
+            <Section classname={"h-screen"} sectionId={"experience"}>
+                Experience
+            </Section>
         </DefaultLayout>
     );
+};
+
+Homepage.propTypes = {
+    title: PropTypes.string,
 };
 
 export default Homepage;
