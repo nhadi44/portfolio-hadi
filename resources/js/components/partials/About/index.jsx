294532208155fa64a8f88e86356ff098ce8ac11d
nhadi44 <hadinurhidayat97@hotmail.com>
2023-12-29 00:35:48 +0700

    update about section

--- resources/js/components/partials/About/index.jsx
+++ resources/js/components/partials/About/index.jsx
@@ -1,7 +1,33 @@
 const About = () => {
     return (
-        <div className="text-white">
-            <h1>About</h1>
+        <div className="about">
+            <h1 className="about__title">About Me</h1>
+            <div className="about__content">
+                <div className="col-span-1">
+                    <p className="about__paragraph">
+                        Hello! My name is Hadi Nurhidayat. I am very interested
+                        in web development, starting in 2020. I first became
+                        interested because, when I worked at an insurance
+                        company, I was given the challenge of creating a
+                        website-based employee productivity system by my boss.
+                    </p>
+                    <p>
+                        Since then, I have started to relearn the basics of web
+                        programming, such as HTML, CSS, Javascript, and PHP.
+                    </p>
+                    <p>
+                        Not only that, I also attended a bootcamp to deepen my
+                        skills in the world of web development .Until now, I
+                        have worked at Indocyber Global Technology as a
+                        full-stack developer.
+                    </p>
+                    <p>
+                        The skills that I currently master are: Javascript,
+                        ReactJS, Node.js, Laravel, and VueJS
+                    </p>
+                </div>
+                <div className="col-span-1">right</div>
+            </div>
         </div>
     );
 };
